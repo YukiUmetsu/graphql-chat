2fb3a4cff046ba324351dc86a79c2ee1844b7f6a
Yuki Umetsu <yuuki.umetsu@gmail.com>
2021-05-09 17:32:40 -0500

    use react hooks to get messages

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,43 +1,57 @@
-import React, { Component } from 'react';
-import { addMessage, getMessages, onMessageAdded } from './graphql/queries';
+import React, {useState} from 'react';
+import { useQuery, useMutation, useSubscription } from '@apollo/react-hooks';
+import { messagesQuery, addMessageMutation, messageAddedSubscription } from './graphql/queries';
 import MessageInput from './MessageInput';
 import MessageList from './MessageList';
+import Loader from "react-loader-spinner";
 
-class Chat extends Component {
-  state = {messages: []};
-  subscription = null;
-
-  async componentDidMount() {
-    const messages = await getMessages();
-    this.setState({messages});
-    this.subscription = onMessageAdded((message) => {
-      this.setState({messages: this.state.messages.concat(message)});
-    });
-  }
-
-  componentWillUnmount() {
-    if (this.subscription) { 
-      this.subscription.unsubscribe();
+const Chat = ({user}) => {
+  const [messages, setMessages] = useState([]);
+  // get messages on load
+  const {loading, error} = useQuery(messagesQuery, {
+    onCompleted: (data) => setMessages(data.messages)
+  });
+  
+  // get on going messages after first load
+  useSubscription(messageAddedSubscription, {
+    onSubscriptionData: ({subscriptionData}) => {
+      setMessages(messages.concat(subscriptionData.data.messageAdded));
     }
+  });
+
+  // add user typed new messages and distribute
+  const [addMessage] = useMutation(addMessageMutation);
+  const handleSend = async (text) => {
+    await addMessage({variables: {input: {text}}});
   }
 
-  async handleSend(text) {
-    await addMessage(text);
-  }
-
-  render() {
-    const {user} = this.props;
-    const {messages} = this.state;
+  if (loading) {
     return (
-      <section className="section">
-        <div className="container">
-          <h1 className="title">Chatting as {user}</h1>
-          <MessageList user={user} messages={messages} />
-          <MessageInput onSend={this.handleSend.bind(this)} />
+      <div className="has-text-centered"><Loader type="Bars" color="#00BFFF" height={80} width={80} /></div>)
+  }
+  if (error) {
+    return (
+      <article class="message is-warning has-text-centered">
+        <div class="message-header">
+          <p>Warning</p>
+          <button class="delete" aria-label="delete"></button>
         </div>
-      </section>
-    );
-  }  
+        <div class="message-body">
+          {error.message}
+        </div>
+      </article>
+    )
+  }
+
+  return (
+    <section className="section">
+      <div className="container">
+        <h1 className="title">Chatting as {user}</h1>
+        <MessageList user={user} messages={messages} />
+        <MessageInput onSend={handleSend} />
+      </div>
+    </section>
+  );
 }
 
 export default Chat;
--- client/src/graphql/queries.js
+++ client/src/graphql/queries.js
@@ -1,7 +1,7 @@
 import gql from 'graphql-tag';
 import client from './client';
 
-const messagesQuery = gql`
+export const messagesQuery = gql`
   query MessagesQuery {
     messages {
       id
@@ -11,7 +11,7 @@ const messagesQuery = gql`
   }
 `;
 
-const addMessageMutation = gql`
+export const addMessageMutation = gql`
   mutation AddMessageMutation($input: MessageInput!) {
     message: addMessage(input: $input) {
       id
@@ -34,7 +34,7 @@ export async function getMessages() {
   return data.messages;
 }
 
-const messageAddedSubscription = gql`
+export const messageAddedSubscription = gql`
   subscription {
     messageAdded {
       id
