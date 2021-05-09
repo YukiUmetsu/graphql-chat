e7329fd4ac1c727722db82d45d9674366ceb7036
Yuki Umetsu <yuuki.umetsu@gmail.com>
2021-05-08 23:13:27 -0500

    add subscription environment

--- client/src/graphql/client.js
+++ client/src/graphql/client.js
@@ -1,9 +1,11 @@
 import {
-  ApolloClient, ApolloLink, HttpLink, InMemoryCache
+  ApolloClient, ApolloLink, HttpLink, InMemoryCache, split,
 } from 'apollo-boost';
+import { WebSocketLink } from 'apollo-link-ws';
 import { getAccessToken } from '../auth';
 
 const httpUrl = 'http://localhost:9000/graphql';
+const wsUrl = 'ws://localhost:9000/graphql';
 
 const httpLink = ApolloLink.from([
   new ApolloLink((operation, forward) => {
@@ -16,9 +18,19 @@ const httpLink = ApolloLink.from([
   new HttpLink({uri: httpUrl})
 ]);
 
+const wsLink = new WebSocketLink({uri: wsUrl, options: {
+  lazy: true,
+  reconnect: true,
+}});
+
+function isSubscription(operation) {
+  const definition = getMainDefinition(operation.query);
+  return definition.kind === 'OperationDefinition' && definition.operation === 'subscription';
+}
+
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: httpLink,
+  link: split(isSubscription, wsLink, httpLink),
   defaultOptions: {query: {fetchPolicy: 'no-cache'}}
 });
 
